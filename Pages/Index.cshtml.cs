ba7f0d5881c051e5fd06d7f453a315c1572ff9f2
Rikke Barsten <rbar@regsj.intern>
2017-10-30 09:46:37 +0100

    First take at view using DI

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -4,14 +4,28 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using AZ_project.Model;
 
 namespace AZ_project.Pages
 {
     public class IndexModel : PageModel
     {
+        
+        private IEmployeeAntalRepository _employeeAntalRepo;
+        public List<EmployeeAntal> EmployeeAntal { get; set; }
+
+        public IndexModel(IEmployeeAntalRepository userRepo)
+        {
+            _employeeAntalRepo = userRepo;
+        }
+
+        public string Message { get; set; }
+        
+        
         public void OnGet()
         {
-
+            EmployeeAntal = _employeeAntalRepo.GetAll();
+            Message = "Test";
         }
     }
 }
--- Startup.cs
+++ Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using AZ_project.Model;
 
 namespace AZ_project
 {
@@ -22,6 +23,10 @@ namespace AZ_project
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+
+            //From here: https://exceptionnotfound.net/asp-net-core-demystified-razor-pages/
+            services.AddTransient<IEmployeeAntalRepository, EmployeeAntalRepository>();
+          
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
