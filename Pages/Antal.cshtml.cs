7b25a1a844a2f2916a784daf7b0720b48d63f4de
Rikke Barsten <rbar@regsj.intern>
2017-11-03 09:42:33 +0100

    Create new page for Antal

---
+++ Pages/Antal.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using AZ_project.Model;
+
+namespace AZ_project.Pages
+{
+    public class AntalModel : PageModel
+    {
+        private IEmployeeAntalRepository _employeeAntalRepo;
+        public List<EmployeeAntal> EmployeeAntal { get; set; }
+
+        public AntalModel(IEmployeeAntalRepository userRepo)
+        {
+            _employeeAntalRepo = userRepo;
+        }
+
+        public string Message { get; set; }
+        
+        
+        public void OnGet()
+        {
+            EmployeeAntal = _employeeAntalRepo.GetAll();
+            Message = "Test";
+        }
+    }
+}
